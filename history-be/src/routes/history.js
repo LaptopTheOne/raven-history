c61350e04a778b839d74204b5457051315e0b31e
LaptopTheOne <moneywithin@yahoo.com>
2023-03-11 19:32:49 +0100

    Added new api endpoint to acquire bearer token - boilerplate.

--- history-be/src/routes/history.js
+++ history-be/src/routes/history.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const https = require('https');
 
 const express = require('express');
 const router = express.Router();
 
 const products = [];
+
+router.get('/bearer-token', (req, res, next) => {
+  console.log('acquiring bearer token for history scope...');
+  console.log('req params', req.query)
+  res.write('test');
+  res.end();
+});
 
 router.get('/get-saved-items', (req, res, next) => {
   console.log(req)
