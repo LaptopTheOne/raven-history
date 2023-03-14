527efaff52255a051b5968922d4da5bf05412c33
LaptopTheOne <moneywithin@yahoo.com>
2023-03-14 16:14:09 +0100

    Angular web component - updated the inputs.

--- history-ui/projects/history-table/src/lib/history-table.component.ts
+++ history-ui/projects/history-table/src/lib/history-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -7,13 +7,11 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   styleUrls: ['./history-table.component.css']
 })
 export class HistoryTableComponent implements OnInit {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  @Input()
+  savedItems: any = [];
+  done: any = [];
 
-  public ngOnInit(): void {
-    console.log('ToDo ', this.todo);
-    console.log('Done ', this.done);
-  }
+  public ngOnInit(): void { }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
